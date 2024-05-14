671fac6f4de564ac7457b7f816ff4514d369a295
olifog <oliverfogelin@gmail.com>
2024-05-14 11:57:01 +0100

    just use native fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,6 @@
 
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
 import 'react-native-reanimated';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
@@ -10,20 +8,6 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/Roboto-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
     <SafeAreaProvider>
       <Stack>
