0090a3a0939e13203929d2c2ec82cfb1286ab6f7
andyzhu23 <andy.zhu23@stgeorges.bc.ca>
2024-05-14 15:43:49 +0100

    Added blog styles to help page

--- app/(tabs)/help.tsx
+++ app/(tabs)/help.tsx
@@ -1,23 +1,25 @@
 import { Text, Button, View } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { TextBlog } from "@/components/TextBlog";
+import { LinearTransition } from "react-native-reanimated";
+
 
 export default function HelpScreen() {
   return (
-    <View>
-      <View style={{alignItems:"center"}}>
-        <View><Text>HELP</Text></View>
-        <View><Text>FAQs</Text></View>
-        <View><Text>How do I input my location?</Text></View>
-        <View><Text>What does setting thresholds do?</Text></View>
-        <View><Text>What’s unique about this app?</Text></View>
-        <View><Text>How do I change my routes?</Text></View>
-        <View><Text>Any other Questions? Contact our Support Team!</Text></View>
-        <View><Text>Phone:  +86 (0)1223 337733
-            Email: Support@weathergpt.ai
-            Postal Address:
-            1600 Pennsylvania Avenue NW, Washington, DC 20500, USA
-            </Text>
-        </View>
+    
+    <View style={{
+    }}>
+      <TextBlog text = {"HELP"} hidden = {""} bold = {true} />
+      <TextBlog text = {"FAQs"} hidden = {""} />
+      <TextBlog text = {"How do I input my location?"} hidden = {""} />
+      <TextBlog text = {"What does setting thresholds do?"} hidden = {""} />
+      <TextBlog text = {"What’s unique about this app?"} hidden = {""} />
+      <TextBlog text = {"How do I change my routes?"} hidden = {""} />
+      <TextBlog text = {"Any other Questions?\n Contact our Support Team!"} 
+        hidden = {"Phone:  +86 (0)1223 337733\nEmail: Support@weathergpt.ai\nPostal Address:\n1600 Pennsylvania Avenue NW, Washington, DC 20500, USA"} 
+        bold = {true}/>
+      <View><Text>
+          </Text>
       </View>
     </View>
   );
---
+++ components/TextBlog.tsx
@@ -0,0 +1,37 @@
+import { View, Text } from "react-native";
+
+export const TextBlog = ({
+  text,
+  hidden,
+  bold = false
+}: {
+    text: string
+    hidden: string
+    bold?: boolean
+}) => {
+
+  return (
+    <Text style = {{textAlign: "center"}}>
+      <View style = {{
+        height:50,
+        width:250,
+        borderColor: "black",
+        borderWidth: 0,
+        margin: 10,
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <Text style = {{
+          textAlign: "center", 
+          fontSize: 15,
+          fontWeight: bold ? 'bold' : 'normal',
+          
+          }}>{text}</Text>
+        <Text>{hidden}</Text>
+      </View>
+    </Text>
+  )
+}
