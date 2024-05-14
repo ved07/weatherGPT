d1cc40ceb72c144e3640f0692f9195ef805b3a1e
andyzhu23 <andy.zhu23@stgeorges.bc.ca>
2024-05-14 14:40:55 +0100

    a

--- app/(tabs)/help.tsx
+++ app/(tabs)/help.tsx
@@ -1,11 +1,24 @@
-import { Text, Button } from "react-native";
+import { Text, Button, View } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HelpScreen() {
   return (
-    <SafeAreaView className="bg-gray-200">
-      <Text className="text-blue-200">Hi this is a test</Text>
-      <Button color="#FF0000" title="Click me"/>
-    </SafeAreaView>
+    <View>
+      <View style={{alignItems:"center"}}>
+        <View><Text>HELP</Text></View>
+        <View><Text>FAQs</Text></View>
+        <View><Text>How do I input my location?</Text></View>
+        <View><Text>What does setting thresholds do?</Text></View>
+        <View><Text>What’s unique about this app?</Text></View>
+        <View><Text>How do I change my routes?</Text></View>
+        <View><Text>Any other Questions? Contact our Support Team!</Text></View>
+        <View><Text>Phone:  +86 (0)1223 337733
+            Email: Support@weathergpt.ai
+            Postal Address:
+            1600 Pennsylvania Avenue NW, Washington, DC 20500, USA
+            </Text>
+        </View>
+      </View>
+    </View>
   );
 }
