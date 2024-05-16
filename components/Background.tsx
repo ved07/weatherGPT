ad9dcb8d630b54c979d28bf94acbb02b0a707360
andyzhu23 <andy.zheyuan.zhu@gmail.com>
2024-05-16 16:20:11 +0100

    added sun and cloud animation

--- components/Background.tsx
+++ components/Background.tsx
@@ -6,30 +6,48 @@ interface BackgroundProps {
     children: ReactNode;
 }
 
-var tree_position = 100;
-var cyclist_position = 0;
-var sun_position = 0;
-var cloud_position = 0;
-
 export const Background = ({ children } : BackgroundProps) => {
-  const [cyclist_position, setCyclistPosition] = useState(0)
-  const cyclistPositionRef = useRef(0)
+  const [cyclist_position, setCyclistPosition] = useState(100)
+  const cyclistPositionRef = useRef(99)
   const [tree_position, setTreePosition] = useState(0)
   const treePositionRef = useRef(0)
+  const [sun_position_x, setSunPositionX] = useState(0)
+  const sunposition_xRef = useRef(0)
+  const [cloud_position_x, setCloudPositionX] = useState(66)
+  const cloud_position_xRef = useRef(66)
+  const [cloud_position_y, setCloudPositionY] = useState(0)
+  const cloud_position_yRef = useRef(0)
+
+  const tmp = useRef(0);
+  const tmp2 = useRef(0);
 
   useEffect(() => {
     const rerender = () => {
       setCyclistPosition(cyclistPositionRef.current);
-      if(cyclistPositionRef.current > 100) {
-        cyclistPositionRef.current -= 10
-      } else if(cyclistPositionRef) {
-        cyclistPositionRef.current += 10
+      if(cyclistPositionRef.current < 100) {
+        tmp2.current = 1
+      } else if(cyclistPositionRef.current > 110) {
+        tmp2.current = -1
       }
+      cyclistPositionRef.current += tmp2.current;
+      
       setTreePosition(treePositionRef.current - 1);
       treePositionRef.current -= 10
       if(treePositionRef.current < -300) {
         treePositionRef.current = 1000
       }
+
+      setCloudPositionX(cloud_position_xRef.current);
+      setCloudPositionY(cloud_position_yRef.current);
+      if(cloud_position_xRef.current < 67) {
+        tmp.current = 1;
+      } else if(cloud_position_xRef.current > 70) {
+        tmp.current = -1
+      }
+      cloud_position_xRef.current += tmp.current;
+      cloud_position_yRef.current += tmp.current;
+      sunposition_xRef.current += tmp.current;
+      setSunPositionX(sunposition_xRef.current);
 
       console.log()
 
@@ -45,8 +63,8 @@ export const Background = ({ children } : BackgroundProps) => {
           <Image
               source={require('./../assets/sun.png')}
               style={{ 
-                left: sun_position, 
-                top: sun_position,
+                left: sun_position_x, 
+                top: 0,
                 marginLeft: 0, 
                 flex: 1, 
                 resizeMode: 'contain', 
@@ -57,8 +75,9 @@ export const Background = ({ children } : BackgroundProps) => {
           <Image
               source={require('./../assets/cloud.png')}
               
-              style={{ 
-                left:66,
+              style={{
+                left:cloud_position_x,
+                top: cloud_position_y,
                 marginLeft: 1, 
                 flex: 2, 
                 resizeMode: 'contain', 
