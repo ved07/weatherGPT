7ccc5258d0901885c89c7b760961980c5a0b1b97
andyzhu23 <andy.zheyuan.zhu@gmail.com>
2024-05-16 15:23:35 +0100

    added cyclist, tree and road

--- components/Background.tsx
+++ components/Background.tsx
@@ -12,11 +12,35 @@ const TopGraphic = () => {
       <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
           <Image
               source={require('./../assets/sun.png')}
-              style={{ borderLeftWidth: 0, marginLeft: 0, flex: 1, resizeMode: 'contain', aspectRatio: 1 }}
+              style={{ left: 0, marginLeft: 0, flex: 1, resizeMode: 'contain', aspectRatio: 1 }}
           />
           <Image
               source={require('./../assets/cloud.png')}
               style={{ marginLeft: 1, flex: 2, resizeMode: 'contain', aspectRatio: 3 }}
+          />
+          <Image
+            source = {require('./../assets/tree.png')}
+            style = {{
+              alignContent: "center",
+              position: "absolute",
+              top: 100
+            }}
+          />
+          <Image
+            source = {require('./../assets/cyclist.png')}
+            style = {{
+              alignContent:"center",
+              position: "absolute",
+              top: 160
+            }}
+          />
+          <Image
+            source = {require('./../assets/road.png')}
+            style = {{
+              alignContent:"center",
+              position: "absolute",
+              top: 334
+            }}
           />
       </View>
   );
@@ -28,7 +52,7 @@ export const Background = ({ children } : BackgroundProps) => {
         <TopGraphic/>
         <View style = {{
           backgroundColor: "#0FC100",
-          marginTop: 270,
+          marginTop: 350,
           height: 700,
           ...StyleSheet.absoluteFillObject,
         }}/>
