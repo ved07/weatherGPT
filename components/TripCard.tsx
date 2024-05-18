6946d8f0e423ae604ba1b3ef9e8c5f06822f6bf7
jessica4763 <jessicawang4763@gmail.com>
2024-05-18 14:16:12 +0100

    added more images to tripcard

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -8,6 +8,10 @@ export interface LocationTime {
 
 const LocationTimeDisplay = ({location, time}: LocationTime) => (
   <View className="flex flex-col items-center justify-center bg-gray-100 w-32 h-32">
+    <Image
+      source = {require('./../assets/house.png')}
+      style = {{ aspectRatio: 1, width: '25%', height: "auto"}}
+    />
     <View><Text className="text-center">{location}</Text></View>
     <View><Text className="text-lg">{time}</Text></View>
   </View>
@@ -32,11 +36,17 @@ export const TripCard = ({
       <LocationTimeDisplay {...start} />
       <View className="items-center p-4">
         <Image
+          source = {require('./../assets/rightarrow.png')}
+          style = {{ aspectRatio: 1, width: '50%', height: "auto"}}
+        />
+        <Image
           source = {require('./../assets/thermometer.png')}
+          style = {{ aspectRatio: 1, width: '50%', height: "auto"}}
         />
         <Text>{temperature.toFixed(1)}°C</Text>
         <Image
           source = {require('./../assets/rainycloud.png')}
+          style = {{ aspectRatio: 1, width: '50%', height: "auto"}}
         />
         <Text>{rainfall.toFixed(1)}mm</Text>
       </View>
