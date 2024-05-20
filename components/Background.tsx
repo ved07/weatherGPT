3630ecd6418296b822e26e398f4b154dba72ebff
andyzhu23 <andy.zheyuan.zhu@gmail.com>
2024-05-20 20:00:33 +0100

    updated road

--- components/Background.tsx
+++ components/Background.tsx
@@ -21,6 +21,21 @@ export const Background = ({ children, plan } : BackgroundProps) => {
   const [cloud_position_y, setCloudPositionY] = useState(-100)
   const cloud_position_yRef = useRef(-100)
 
+  const [pos1, setPos1] = useState(0)
+  const pos1Ref = useRef(0)
+  const [pos2, setPos2] = useState(60)
+  const pos2Ref = useRef(60)
+  const [pos3, setPos3] = useState(120)
+  const pos3Ref = useRef(120)
+  const [pos4, setPos4] = useState(180)
+  const pos4Ref = useRef(180)
+  const [pos5, setPos5] = useState(240)
+  const pos5Ref = useRef(240)
+  const [pos6, setPos6] = useState(300)
+  const pos6Ref = useRef(300)
+  const [pos7, setPos7] = useState(360)
+  const pos7Ref = useRef(360)
+
   const tmp = useRef(0);
   const tmp2 = useRef(0);
 
@@ -41,6 +56,27 @@ export const Background = ({ children, plan } : BackgroundProps) => {
       
       setTreePosition(treePositionRef.current - 1);
       treePositionRef.current -= 10
+      pos1Ref.current -= 10
+      pos2Ref.current -= 10
+      pos3Ref.current -= 10
+      pos4Ref.current -= 10
+      pos5Ref.current -= 10
+      pos6Ref.current -= 10
+      pos7Ref.current -= 10
+      if(pos1Ref.current <= -20) pos1Ref.current = 400
+      if(pos2Ref.current <= -20) pos2Ref.current = 400
+      if(pos3Ref.current <= -20) pos3Ref.current = 400
+      if(pos4Ref.current <= -20) pos4Ref.current = 400
+      if(pos5Ref.current <= -20) pos5Ref.current = 400
+      if(pos6Ref.current <= -20) pos6Ref.current = 400
+      if(pos7Ref.current <= -20) pos7Ref.current = 400
+      setPos1(pos1Ref.current)
+      setPos2(pos2Ref.current)
+      setPos3(pos3Ref.current)
+      setPos4(pos4Ref.current)
+      setPos5(pos5Ref.current)
+      setPos6(pos6Ref.current)
+      setPos7(pos7Ref.current)
       if(treePositionRef.current < -300) {
         treePositionRef.current = 500
       }
@@ -99,6 +135,69 @@ export const Background = ({ children, plan } : BackgroundProps) => {
               alignContent:"center",
               position: "absolute",
               top: 334,
+            }}
+          />
+          <Image
+            source = {require('./../assets/mark.png')}
+            style = {{
+              alignContent:"center",
+              position: "absolute",
+              top: 334,
+              left: pos1
+            }}
+          />
+          <Image
+            source = {require('./../assets/mark.png')}
+            style = {{
+              alignContent:"center",
+              position: "absolute",
+              top: 334,
+              left: pos2
+            }}
+          />
+          <Image
+            source = {require('./../assets/mark.png')}
+            style = {{
+              alignContent:"center",
+              position: "absolute",
+              top: 334,
+              left: pos3
+            }}
+          />
+          <Image
+            source = {require('./../assets/mark.png')}
+            style = {{
+              alignContent:"center",
+              position: "absolute",
+              top: 334,
+              left: pos4
+            }}
+          />
+          <Image
+            source = {require('./../assets/mark.png')}
+            style = {{
+              alignContent:"center",
+              position: "absolute",
+              top: 334,
+              left: pos5
+            }}
+          />
+          <Image
+            source = {require('./../assets/mark.png')}
+            style = {{
+              alignContent:"center",
+              position: "absolute",
+              top: 334,
+              left: pos6
+            }}
+          />
+          <Image
+            source = {require('./../assets/mark.png')}
+            style = {{
+              alignContent:"center",
+              position: "absolute",
+              top: 334,
+              left: pos7
             }}
           />
         </View>
