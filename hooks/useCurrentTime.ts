0e600c1a36e2801faa920e609781434664cf7e10
olifog <oliverfogelin@gmail.com>
2024-05-20 19:59:12 +0100

    getDayOfWeek

--- hooks/useCurrentTime.ts
+++ hooks/useCurrentTime.ts
@@ -3,3 +3,7 @@ export const useCurrentTime = () => {
   // manually set to 13:00pm on today's date for testing
   return new Date(new Date().setHours(13, 0, 0, 0));
 }
+
+export const getDayOfWeek = (date: Date): number => {
+  return (date.getDay() + 6) % 7
+}
