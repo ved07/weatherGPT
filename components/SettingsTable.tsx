f6222afa1d2077aabc25ec136c8af390d955c425
Vedaangh Rungta <vedaangh.rungta@gmail.com>
2024-05-20 20:34:49 +0100

    Commiting table in one big update, according to proper principles.

--- components/SettingsTable.tsx
+++ components/SettingsTable.tsx
@@ -1,44 +1,61 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, FlatList} from 'react-native';
+import { View, Text, TextInput,Pressable, StyleSheet, FlatList} from 'react-native';
 import BouncyCheckbox from "react-native-bouncy-checkbox";
+import RNDateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
 interface stitem {
     id: string, 
     day: string, 
     startTime: string,
     endTime: string,
     location: string,
-    ticked: boolean
+    ticked: boolean,
+    date: Date
 }
 
 /* */
 const SettingsTable: React.FC = () => {
+    const [showTimeSelect, setShowTimeSelect] = useState(false);
+    const [startEndFlag, setstartEndFlag] = useState('e');
+    const [day, setDay] = useState(0);
+    const [twofourhour, setTwoFourHour] = useState(true);
     const [stData, setStData] = useState<stitem[]>([
-        { id: '1', day: 'Mon', startTime: '08:00', endTime: '16:00', location: 'Office', ticked: true },
-        { id: '2', day: 'Tue', startTime: '08:00', endTime: '16:00', location: 'Home', ticked: true },
-        { id: '3', day: 'Wed', startTime: '08:00', endTime: '16:00', location: 'Gym', ticked: true },
-        { id: '4', day: 'Thu', startTime: '08:00', endTime: '16:00', location: 'Client Site', ticked: true },
-        { id: '5', day: 'Fri', startTime: '08:00', endTime: '16:00', location: 'Coffee Shop', ticked: true },
-        { id: '6', day: 'Sat', startTime: '10:00', endTime: '14:00', location: 'Weekend Market', ticked: true },
-        { id: '7', day: 'Sun', startTime: '12:00', endTime: '18:00', location: 'Beach', ticked: true }
+        { id: '1', day: 'Mon', startTime: '08:00', endTime: '16:00', location: 'Office', ticked: true, date: new Date() },
+        { id: '2', day: 'Tue', startTime: '08:00', endTime: '16:00', location: 'Home', ticked: true, date: new Date() },
+        { id: '3', day: 'Wed', startTime: '08:00', endTime: '16:00', location: 'Gym', ticked: true, date: new Date() },
+        { id: '4', day: 'Thu', startTime: '08:00', endTime: '16:00', location: 'Client Site', ticked: true, date: new Date() },
+        { id: '5', day: 'Fri', startTime: '08:00', endTime: '16:00', location: 'Coffee Shop', ticked: true,date: new Date() },
+        { id: '6', day: 'Sat', startTime: '10:00', endTime: '14:00', location: 'Weekend Market', ticked: true,date: new Date()},
+        { id: '7', day: 'Sun', startTime: '12:00', endTime: '18:00', location: 'Beach', ticked: true, date: new Date()}
     ])
-    
+    const formatString = (sNum: number) => {
+      return (('0' + sNum.toString()).slice(-2))
+    }
     const handleCheckboxChange = (index: number) => {
         const newStData = [...stData];
         newStData[index].ticked = !newStData[index].ticked;
         setStData(newStData);
     }
     
-    const handleTimeChange = (time1: string, index:number, flag='s') => {
+    const handleTimeChange = (index:number, flag='s') => {
         const newStData = [...stData];
-        if (flag=='s'){
-            newStData[index].startTime = time1;
-            setStData(newStData);   
-        }
-        else {
-            newStData[index].endTime = time1;
-            setStData(newStData); 
-        }   
+        setstartEndFlag(flag);
+        setDay(index);  
+        setShowTimeSelect(true);  
     }
+    const setTime = (event: DateTimePickerEvent, date?: Date) => {
+      setShowTimeSelect(false);
+      if (event.type == 'set' && typeof date != 'undefined'){
+        const newStData = [...stData];
+      if (startEndFlag=='s'){
+          newStData[day].startTime = `${formatString(date.getUTCHours())}:${formatString(date.getUTCMinutes())}`;
+      }
+      else {
+          newStData[day].endTime = `${formatString(date.getUTCHours())}:${formatString(date.getUTCMinutes())}`;
+      }   
+      newStData[day].date = date;
+      setStData(newStData); 
+      }
+  }
     const handleLocationChange = (location: string, index:number) => {
         const newStData = [...stData];
         newStData[index].location  = location;
@@ -52,18 +69,17 @@ const SettingsTable: React.FC = () => {
             isChecked={item.ticked}
             onPress={() => handleCheckboxChange(index)}
           />
-          <TextInput
+          <Pressable
             style={styles.input}
-            value={item.startTime}
-            onChangeText={(text) => handleTimeChange(text, index)}
-            keyboardType="numeric"
-          />
-          <TextInput
+            
+            onPress={() => handleTimeChange(index)}
+          ><Text>{item.startTime}</Text></Pressable>
+          <Pressable
             style={styles.input}
-            value={item.endTime}
-            onChangeText={(text) => handleTimeChange(text, index, 'e')}
-            keyboardType="numeric"
-          />
+            
+            onPress={() => handleTimeChange(index, 'e')}
+          ><Text>{item.endTime}</Text></Pressable>
+          
           <TextInput
             style={styles.input}
             value={item.location}
@@ -81,6 +97,7 @@ const SettingsTable: React.FC = () => {
               renderItem={renderitem}
               keyExtractor={(item, index) => item.id}
             />
+            {showTimeSelect && <RNDateTimePicker value={new Date(2020, 10, 20,)} mode="time" display = "spinner" onChange={setTime} is24Hour = {twofourhour}/>}
         </View>
       );
     
